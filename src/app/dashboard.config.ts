518dc86abd0479f5f135173c16634366383ae419
lturmel <lturmel@expertasolutions.com>
2019-03-06 19:58:00 -0500

    Fix try

--- src/app/dashboard.config.ts
+++ src/app/dashboard.config.ts
@@ -2,13 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { IDashboardConfig } from './dashboardConfig.model';
+import * as fs from "file-system";
+
 @Injectable()
 export class DashboardConfig {
   static settings: IDashboardConfig;
   constructor(private http: HttpClient){}
   load(){
-     const jsonFile = 'secrets/vstsDashboard.secret.release.json';
-    return new Promise<void>((resolve, reject) => {
+     const jsonFile = '/app/secrets/vstsDashboard.secret.release.json';
+     console.log(jsonFile);
+     var content = fs.File.fromPath(jsonFile);
+     console.log(content);
+     return new Promise<void>((resolve, reject) => {
       this.http.get(jsonFile).toPromise().then((response : IDashboardConfig) => {
          DashboardConfig.settings = <IDashboardConfig>response;
          resolve();
