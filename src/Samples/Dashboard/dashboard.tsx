527a9f2d3f748486bea9790c6210216ea81dc653
lturmel <lturmel@expertasolutions.com>
2020-01-01 13:31:28 -0500

    test
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/Samples/Dashboard/dashboard.tsx
+++ src/Samples/Dashboard/dashboard.tsx
@@ -144,7 +144,9 @@ class CICDDashboard extends React.Component<{}, {}> {
                 onSelect={(event, data) => {
                   this.currentSelectedBuildReferenceId.value = data.data.id;
                   this.isDialogOpen.value = true;
-                  this.buildItemProvider = new ObservableValue<ArrayItemProvider<Build>>(new ArrayItemProvider(this.state.builds.filter(x=> x.definition.id === this.currentSelectedBuildReferenceId.value)));
+                  this.buildItemProvider = new ObservableValue<ArrayItemProvider<Build>>(
+                    new ArrayItemProvider(this.state.builds.filter(x=> x.definition.id === this.currentSelectedBuildReferenceId.value))
+                  );
                   console.log("Selected Row - " + data.index)
                 }}
                 onActivate={(event, row) => console.log("Activated Row - " + row.index)}
