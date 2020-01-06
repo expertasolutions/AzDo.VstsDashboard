38b828740a04b7c7b5ea61b1b0b43d56e818caf3
lturmel <lturmel@expertasolutions.com>
2020-01-05 20:54:53 -0500

    remove filter
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/Samples/Dashboard/PipelineServices.tsx
+++ src/Samples/Dashboard/PipelineServices.tsx
@@ -21,7 +21,7 @@ export async function getReleases(projectName: string) {
   let releaseClient = API.getClient(ReleaseRestClient);
   return await releaseClient.getDeployments(projectName, undefined, undefined, undefined,undefined, undefined,
                                             undefined, undefined,undefined,undefined, undefined, undefined,
-                                            undefined, new Date("2000-01-01"), new Date(),undefined);
+                                            undefined, undefined, undefined, undefined);
 }
 
 export async function getBuilds(projectName: string)  {
