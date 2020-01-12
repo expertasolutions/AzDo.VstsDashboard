20131d4996b218cb9dbd6cabfb6922b2b2121988
lturmel <lturmel@expertasolutions.com>
2020-01-12 13:07:11 -0500

    remove log
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/Samples/Dashboard/PipelineServices.tsx
+++ src/Samples/Dashboard/PipelineServices.tsx
@@ -19,10 +19,8 @@ export async function getProjects() {
 
 export async function getReleases(projectName: string) {
   let minDate = new Date();
-  console.log("CurrentDate: " + minDate);
   let newDate = minDate.setDate(minDate.getDate()-365);
   minDate = new Date(newDate);
-  console.log("MinDate: " + minDate);
   let releaseClient = API.getClient(ReleaseRestClient);
   return await releaseClient.getDeployments(projectName, undefined, undefined, undefined, minDate, new Date(),
     undefined, undefined, true, undefined, -1, undefined,
