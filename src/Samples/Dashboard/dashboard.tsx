b52456dbe52fddabcd94225532e644b494cb4fc9
lturmel <lturmel@expertasolutions.com>
2020-01-16 12:20:55 -0500

    project sorting
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/Samples/Dashboard/dashboard.tsx
+++ src/Samples/Dashboard/dashboard.tsx
@@ -99,7 +99,7 @@ class CICDDashboard extends React.Component<{}, {}> {
 
   public loadProjects() {
     getProjects().then(result => {
-      this.setState( { projects: result });
+      this.setState( { projects: result.sort((a,b) => a.name.localeCompare(b.name)) });
       //this.setState( { projects: result.sort((x1, x2) => x1.name > x2.name ? 1: 0) });
     });
     /*
