adedf1b3ef8dae41ebe4e3e9171422ee06029a7a
lturmel <lturmel@expertasolutions.com>
2020-01-16 19:18:32 -0500

    order release env
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/Samples/Dashboard/cells/common.tsx
+++ src/Samples/Dashboard/cells/common.tsx
@@ -190,7 +190,8 @@ export function getReleaseTagFromBuild(build: Build, releases: Array<Deployment>
   for(let relRef=0;relRef<releaseReferences.length;relRef++){
     let relRefInfo = releaseReferences[relRef];
     lastRelease = Array<string>();
-    let releaseDeploys = deploys.filter(x=> x.release.id == relRefInfo.id);
+    let releaseDeploys = deploys.filter(x=> x.release.id == relRefInfo.id)
+                         .sort((a,b)=> a.releaseEnvironment.id - b.releaseEnvironment.id);
 
     for(let i=0;i<releaseDeploys.length;i++) {
       let dep = releaseDeploys[i];
