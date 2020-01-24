b5ceded894ad5c3b255d3b1b0082688ab08cedad
lturmel <lturmel@expertasolutions.com>
2020-01-23 20:51:46 -0500

    top to 1000 releases
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/Samples/Dashboard/PipelineServices.tsx
+++ src/Samples/Dashboard/PipelineServices.tsx
@@ -31,7 +31,7 @@ export async function getReleases(projectName: string) {
   let newDate = minDate.setDate(minDate.getDate()-1000);
   minDate = new Date(newDate);
   return await releaseClient.getDeployments(projectName, undefined, undefined, undefined, minDate, new Date(),
-    undefined, undefined, false, undefined, -1, undefined,
+    undefined, undefined, false, undefined, 1000, undefined,
     undefined, undefined, undefined, undefined);
 }
 
