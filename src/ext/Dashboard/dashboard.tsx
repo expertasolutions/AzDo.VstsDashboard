0e525bd76bea0fb5087b51ad682c64aa0a7704b9
lturmel <lturmel@expertasolutions.com>
2020-02-05 12:40:48 -0500

    remove junk
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/ext/Dashboard/dashboard.tsx
+++ src/ext/Dashboard/dashboard.tsx
@@ -251,7 +251,7 @@ class CICDDashboard extends React.Component<{}, {}> {
         <Page className="pipelines-page flex-grow">
           <Header title="CI/CD Dashboard" titleSize={TitleSize.Large} />
           <div className="page-content-left page-content-right page-content-top">
-            ({this.renderTabBar()})
+            {this.renderTabBar()}
           </div>
           <div className="page-content-left page-content-right page-content-top">
             <FilterBar filter={this.filter}>
