5d60da4620b4597a898212c133709ca2e09c95b5
lturmel <lturmel@expertasolutions.com>
2020-02-04 21:02:36 -0500

    fix ordering
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/ext/Dashboard/PipelineServices.tsx
+++ src/ext/Dashboard/PipelineServices.tsx
@@ -61,14 +61,14 @@ export async function getBuildDefinitions(projectName: string) {
                                               undefined, undefined);
   return result.sort((a,b) => {
     if(b.latestBuild !== undefined && a.latestBuild !== undefined){
-      return -(b.latestBuild.id - a.latestBuild.id);
+      return b.latestBuild.id - a.latestBuild.id;
     } else if(b.latestBuild !== undefined && a.latestBuild === undefined) {
-      return -b.latestBuild.id;
+      return b.latestBuild.id;
     } else if(b.latestBuild === undefined && a.latestBuild !== undefined) {
-      return -a.latestBuild.id;
+      return a.latestBuild.id;
     } else {
       console.log("Not latest build");
     }
-    return a.id;
+    return -1;
   });
 }
