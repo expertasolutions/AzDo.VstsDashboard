4fd4ece23ffadd5db2ad17c538ab84e93d8f317d
lturmel <lturmel@expertasolutions.com>
2020-02-23 21:42:31 -0500

    ui fix
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -267,7 +267,7 @@ export function getReleaseTagFromBuild(build: Build, releases: Array<Deployment>
 
       if(deploys.length > 0) {
         content.push(<div style={{whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}}>
-                        <Link href={relRefInfo._links.web.href} target="_blank"><b>{depName}</b> [{relRefInfo.name}]</Link>
+                        <Link href={relRefInfo._links.web.href} target="_blank"><b>{relRefInfo.name}</b> ({depName})</Link>
                         <p><PillGroup className="flex-row" overflow={PillGroupOverflow.wrap}>{children}</PillGroup></p>
                     </div>);
       }
