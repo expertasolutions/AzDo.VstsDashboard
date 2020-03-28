ce9aac21ee737fa79501cfc964621d2c3c44d333
lturmel <lturmel@expertasolutions.com>
2020-03-28 13:54:19 -0400

    fix
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/ext/Dashboard/dashboard.tsx
+++ src/ext/Dashboard/dashboard.tsx
@@ -200,7 +200,7 @@ class CICDDashboard extends React.Component<{}, {}> {
 
   private onErrorsOnSummaryOnTop = (event: React.SyntheticEvent<HTMLElement>, item: IListBoxItem<{}>) => {
     if(item.id !== undefined) {
-      let showAll = item.id === "yes";
+      let showAll = item.id === "true";
       this.setState({ showErrorsOnSummaryOnTop: showAll });
     } else {
       this.setState({ showErrorsOnSummaryOnTop: true });
