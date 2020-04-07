ccc120d3becbf20132382a53aa51c1b6d8898c60
lturmel <lturmel@expertasolutions.com>
2020-04-07 19:19:34 -0400

    remove output console
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/ext/Dashboard/PipelineServices.tsx
+++ src/ext/Dashboard/PipelineServices.tsx
@@ -186,7 +186,5 @@ export async function getBuildDefinitions(projectName: string, isFirstLoad: bool
                                               undefined, undefined, undefined, undefined, undefined,
                                               undefined, minDate, undefined,undefined, true, undefined, 
                                               undefined, undefined);
-
-  console.log("getBuildDefinitions: " + projectName + " | " + isFirstLoad + " | " + result.length + " build found");
   return result;
 }
