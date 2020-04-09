449b3bdfac6d664f677885be142dc64a51b4685d
lturmel <lturmel@expertasolutions.com>
2020-04-09 10:47:15 -0400

    base on attempt number
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -244,7 +244,8 @@ export function getReleaseTagFromBuild(build: Build, releases: Array<Deployment>
 
       for(let i=0;i<releaseDeploys.length;i++) {
         let dep = releaseDeploys[i];
-        let lastDeploys = releaseDeploys.filter(x=> x.releaseEnvironment.name === dep.releaseEnvironment.name);
+        let lastDeploys = releaseDeploys.filter(x=> x.releaseEnvironment.name === dep.releaseEnvironment.name)
+                          .sort(x=> -x.attempt);
 
         let lastDep = lastDeploys[0];
         let envName = lastDep.releaseEnvironment.name;
