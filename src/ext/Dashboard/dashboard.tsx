bd701dde929169a11df3e80f643c23c5e970056a
lturmel <lturmel@expertasolutions.com>
2020-04-22 18:33:11 -0400

    style update on make a request link
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/ext/Dashboard/dashboard.tsx
+++ src/ext/Dashboard/dashboard.tsx
@@ -501,7 +501,7 @@ class CICDDashboard extends React.Component<{}, {}> {
   public renderOptionsFilterView() : JSX.Element {
     return (
       <div>
-        <Link href="https://github.com/expertasolutions/VstsDashboard/issues/new" target="_blank" subtle={true}>
+        <Link href="https://github.com/expertasolutions/VstsDashboard/issues/new" target="_blank">
           <Icon iconName="FeedbackRequestSolid"/>
         </Link>
         <Link onClick={async ()=> {
