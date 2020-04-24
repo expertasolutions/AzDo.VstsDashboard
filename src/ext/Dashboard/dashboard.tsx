1777e5b97f44deb005b80927c7b7f25098938b3c
lturmel <lturmel@expertasolutions.com>
2020-04-24 10:39:48 -0400

    fix regression
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/ext/Dashboard/dashboard.tsx
+++ src/ext/Dashboard/dashboard.tsx
@@ -706,7 +706,7 @@ class CICDDashboard extends React.Component<{}, {}> {
                             {(props: { selectedTabId: string, refreshUI: string }) => {
                               if(this.state.buildDefs === undefined || this.state.buildDefs.length === 0){
                                 return this.renderZeroData(this.selectedTabId.value);
-                              } else if(this.state.buildDefs.length > 0 && this.state.builds.length === 0) {
+                              } else if(this.state.buildDefs.length > 0 && this.state.builds.length === 0 && props.selectedTabId === "builds") {
                                 return this.renderZeroData(this.selectedTabId.value);
                               } else {
                                 return (
