d1d0ba75f5fcc0afae64bc702dbb0d06f049df04
lturmel <lturmel@expertasolutions.com>
2020-05-10 16:55:13 -0400

    remove build icon
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- src/ext/Dashboard/cells/buildReference.tsx
+++ src/ext/Dashboard/cells/buildReference.tsx
@@ -111,8 +111,7 @@ export function renderLastBuild01 (
     contentRow1 = (<div style={{whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}}>
                     <Status {...getBuildDefinitionStatusNew(lastBuild).statusProps}
                       className="icon-large-margin"
-                      size={StatusSize.s}/>
-                    <Icon iconName="Build"/>&nbsp;<Link href={buildUrl} target="_blank">{lastBuild.buildNumber}</Link>
+                      size={StatusSize.s}/>&nbsp;<Link href={buildUrl} target="_blank">{lastBuild.buildNumber}</Link>
                   </div>);
 
     if(lastBuild.sourceVersion !== undefined) {
