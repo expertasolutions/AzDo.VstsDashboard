f7bc1c1728633742fc0ca13a0449b746bf1cc992
Louis Turmel <lturmel@expertasolutions.com>
2023-10-28 10:00:10 -0400

    update

--- src/ext/Dashboard/PipelineServices.tsx
+++ src/ext/Dashboard/PipelineServices.tsx
@@ -136,7 +136,6 @@ export async function getEnvironments(organization: string, projectName: string,
   for(let i=0;i<result.length;i++) {
       let test = await getEnvironmentDeplRecords(result[i].id, organization, projectName, accessToken);
       console.log(test);
-      console.log(test.definition);
       result.deplRecords.push(...test);
   }
   return result;
