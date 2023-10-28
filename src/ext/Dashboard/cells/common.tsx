4ca90d88fea26913a6612d877a4ab0233732984e
Louis Turmel <lturmel@expertasolutions.com>
2023-10-28 11:05:55 -0400

    test

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -307,13 +307,13 @@ export function getReleaseTagFromBuildV2(build: Build, environments: Array<Pipel
   let allDeplRecords: any[] = [];
   for(let i=0;environments.length;i++) {
     console.log(environments[i]);
-    allDeplRecords.push(...environments[i].deploymentRecords);
+    //allDeplRecords.push(...environments[i].deploymentRecords);
   }
 
   let buildDeplRecords: any[] = allDeplRecords.find(x=> x.definition.id === build.definition.id);
   console.log("buildDeplRecords");
   console.log(buildDeplRecords);
-  
+
   return (<div>Not deployed yet</div>)
 }
 
