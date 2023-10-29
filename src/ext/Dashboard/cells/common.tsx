92beefd7f180f527d5839e69034a5d20073792b5
Louis Turmel <lturmel@expertasolutions.com>
2023-10-29 07:52:34 -0400

    fix

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -363,8 +363,8 @@ export function getReleaseTagFromBuildV2(build: Build, environments: Array<Pipel
   let content: any[] = [];  
   let buildDeplRecords = allDeplRecords.filter(x=> x.owner.id === build.id).sort((a,b) => a.id - b.id);
   if(build.definition.id === 240) {
-    console.log(allDeplRecords);
-    //console.log(buildDeplRecords);
+    //console.log(allDeplRecords);
+    console.log(buildDeplRecords);
   }
   let children: any[] = [];
 
