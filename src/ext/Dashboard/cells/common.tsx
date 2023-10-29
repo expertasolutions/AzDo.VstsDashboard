9f6e69d61abd7961615713011a208997855f81fc
Louis Turmel <lturmel@expertasolutions.com>
2023-10-29 07:00:31 -0400

    test

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -320,7 +320,7 @@ export function getReleaseTagFromBuildV2(build: Build, environments: Array<Pipel
       let deplStatus = getReleaseIndicator(DeploymentStatus.InProgress, false);
       children.push(
         <Pill color={deplStatus.color} variant={PillVariant.colored} 
-            onClick={() => window.open(elm.owner._links.web, "_blank") }>
+            onClick={() => window.open(elm.owner._links.web.href, "_blank") }>
           <Status {...deplStatus.statusProps} className="icon-small-margin" size={StatusSize.s} />&nbsp;{elm.stageName}
         </Pill>
       );     
