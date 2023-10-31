3221433815a020ad30b9a65f5f90d1e08e4cd804
Louis Turmel <lturmel@expertasolutions.com>
2023-10-31 06:40:24 -0400

    fix

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -421,7 +421,7 @@ export function getReleaseTagFromBuildV2(build: Build, environments: Array<Pipel
     let buildDeplRecords = allDeplRecords.filter(x=> x.owner.id === build.id).sort((a,b) => a.id - b.id);
 
     let buildIDApprovals = approvals.filter(x=> x.pipeline !== undefined && x.pipeline.owner.id === build.id);
-    if(buildIDApprovals.length > 0 && build.definition.id ==- 240) {
+    if(buildIDApprovals.length > 0 && build.definition.id === 240) {
       console.log(build.definition.id);
       console.log(buildIDApprovals);
     }
