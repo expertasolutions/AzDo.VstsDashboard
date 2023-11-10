37cf3f835af81f4a9d4f04cb321ef7f8ae38db04
Louis Turmel <lturmel@expertasolutions.com>
2023-11-09 19:44:21 -0500

    update

--- src/ext/Dashboard/cells/buildReference.tsx
+++ src/ext/Dashboard/cells/buildReference.tsx
@@ -280,7 +280,7 @@ export function renderLastBuild02(
 
 export function renderAllInProgress(builds: Array<Build>, context: any) : Array<JSX.Element> {
   let childrens = Array<JSX.Element>();
-  let pending = builds.filter(x=> x.status === BuildStatus.InProgress || x.status === BuildStatus.NotStarted);
+  let pending = builds.filter(x=> x.status === BuildStatus.InProgress);
   console.log(pending);
   for(let i=0;i<pending.length;i++) {
     childrens.push(<div>getReleaseTagFromBuildV2(pending[i], context.state.environments, context.state.approvals, context.state.showAllBuildDeployment)</div>);
