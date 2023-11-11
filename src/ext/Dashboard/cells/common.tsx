f8335643c2631644aaeb541b36a81aa95124bbf9
Louis Turmel <lturmel@expertasolutions.com>
2023-11-11 11:02:34 -0500

    fix

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -542,7 +542,7 @@ export function getEnvironmentStageSummary(build: PipelineInfo, environments: Ar
       <Pill color={envStatus.color} variant={PillVariant.colored} 
         onClick={() => window.open(curEnv.lastExecution.owner._links.web.href, "_blank")}>
         <div className="font-size-s">
-          <Status {...envStatus.statusProps} className="icon-small-margin" size={StatusSize.s} />&nbsp;{curEnv.lastExecution.stageName}&nbsp;{attempCounts} <Icon iconName="Calendar"/>&nbsp;<Ago date={queueDateTime} />&nbsp;<Duration startDate={startDateTime} endDate={endDateTime} /><Icon iconName="Clock"/>
+          <Status {...envStatus.statusProps} className="icon-small-margin" size={StatusSize.s} />&nbsp;{curEnv.lastExecution.stageName}&nbsp;{attempCounts} <Icon iconName="Calendar"/>&nbsp;<Ago date={queueDateTime} />&nbps;<Icon iconName="Clock"/>&nbsp;<Duration startDate={startDateTime} endDate={endDateTime} />
         </div>
         <div style={{ paddingLeft: 16 }} className="font-size-s">{curEnv.lastExecution.owner.name}</div>
       </Pill>
