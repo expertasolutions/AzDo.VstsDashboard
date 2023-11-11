753e5fdc5a0005139d06f6a119e79127d022ba8e
Louis Turmel <lturmel@expertasolutions.com>
2023-11-11 09:11:39 -0500

    paddingLeft fix

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -524,8 +524,8 @@ export function getEnvironmentStageSummary(build: PipelineInfo, environments: Ar
         <div>
           <Status {...envStatus.statusProps} className="icon-small-margin" size={StatusSize.s} />&nbsp;{curEnv.lastExecution.stageName}&nbsp;{attempCounts}
         </div>
-        <div style={{ paddingLeft: 15 }} className="font-size-s">{curEnv.lastExecution.owner.name}</div>
-        <div style={{ paddingLeft: 15, textAlign: "center"}} className="font-size-s">{curEnv.lastExecution.queueTime}</div>
+        <div style={{ paddingLeft: 16 }} className="font-size-s">{curEnv.lastExecution.owner.name}</div>
+        <div style={{ paddingLeft: 16, textAlign: "center"}} className="font-size-s">{curEnv.lastExecution.queueTime}</div>
       </Pill>
     );
   }
