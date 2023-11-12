20380782837ccd6b1edfd85155168255d5de2f8f
Louis Turmel <lturmel@expertasolutions.com>
2023-11-11 22:43:25 -0500

    udpate

--- src/ext/Dashboard/cells/buildReference.tsx
+++ src/ext/Dashboard/cells/buildReference.tsx
@@ -196,8 +196,10 @@ function renderPendingBuild(buildRef:PipelineInfo, buildList: Build[]) {
                 <Link href={buildUrl} target="_blank">{lastBuild.buildNumber}</Link>&nbsp;
               </div>
               <div style={{marginLeft: "10px"}}>
-                <Icon iconName="BranchMerge"/>&nbsp;<Link href={branchUrl} target="_blank">{branchName}</Link>&nbsp;
-                <Icon iconName="BranchCommit" />{sourceVersionCtrl}
+                <div>
+                  <Icon iconName="BranchMerge"/>&nbsp;<Link href={branchUrl} target="_blank">{branchName}</Link>&nbsp;
+                  <Icon iconName="BranchCommit" />{sourceVersionCtrl}
+                </div>
                 <div>
                   {requestByCtrl}&nbsp;{clockCtrl}
                 </div>
