21965a7497ab93a2ca3ae16613f9a636853a8fa0
Louis Turmel <lturmel@expertasolutions.com>
2023-11-23 08:52:06 -0500

    update

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -559,7 +559,7 @@ export function getEnvironmentStageSummary(build: PipelineInfo, environments: Ar
         <div className="font-size-s">
           <Status {...envStatus.statusProps} className="icon-small-margin" size={StatusSize.s} />&nbsp;{curEnv.lastExecution.stageName}&nbsp;{attempCounts}&nbsp;<Icon iconName="Clock"/>&nbsp;<Duration startDate={startDateTime} endDate={endDateTime} />&nbsp;<Icon iconName="Calendar"/>&nbsp;<Ago date={queueDateTime} />
         </div>
-        <div style={{ paddingLeft: 16 }} className="font-size-s">{curEnv.lastExecution.owner.name}</div>
+        <div style={{ paddingLeft: 16 }} className="font-size-s"><Icon iconName="BranchMerge"/>&nbsp;{curEnv.lastExecution.owner.name}</div>
       </Pill>
     );
   }
