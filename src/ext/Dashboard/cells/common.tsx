f234d16a60752e91958d05aa745b811827de4b9a
Louis Turmel <lturmel@expertasolutions.com>
2023-11-23 08:13:21 -0500

    update envOrder

--- src/ext/Dashboard/cells/common.tsx
+++ src/ext/Dashboard/cells/common.tsx
@@ -507,8 +507,9 @@ export function getEnvironmentStageSummary(build: PipelineInfo, environments: Ar
         lastExecution: undefined
       };
 
-      let lastExecution = allDeplRecords.filter(x=> x.definition.id === build.id && x.stageName === envStage)
-                                        .sort((a,b) => a.id - b.id);
+      let lastExecution = allDeplRecords.sort((a,b) => a.id - b.id)
+                                        .filter(x=> x.definition.id === build.id && x.stageName === envStage)
+                                        ;
       if(lastExecution.length > 0) {
         let currentShowed = buildStageEnvironments.find(x=> x.environment === envStage);
         if(currentShowed === undefined || currentShowed.length > 0) {
@@ -524,7 +525,7 @@ export function getEnvironmentStageSummary(build: PipelineInfo, environments: Ar
   }
 
   // Must be Pipeline StageName NOT Environment /// but we have a *** order issue here... !!!
-  buildStageEnvironments = buildStageEnvironments.sort((a,b) => a.lastExecution.id - b.lastExecution.id);
+  buildStageEnvironments = buildStageEnvironments.sort((a,b) => b.lastExecution.id - a.lastExecution.id);
   
   let childrens = Array<any>();
   for(let i=0;i<buildStageEnvironments.length;i++) { 
