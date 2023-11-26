38a2f7cd2f7ce45f1dde6b637758715033b1924e
Louis Turmel <lturmel@expertasolutions.com>
2023-11-26 18:53:07 -0500

    test

--- src/ext/Dashboard/dashboard.tsx
+++ src/ext/Dashboard/dashboard.tsx
@@ -361,6 +361,7 @@ class CICDDashboard extends React.Component<{}, {}> {
             currentResult[buildIndex] = newElement;
             let buildDefs = this.state.buildDefs;
             let buildDef = buildDefs.find(x=> x.id === newElement.definition.id);
+            
             if(buildDef !== undefined && buildDef.latestBuild !== undefined && buildDef.latestBuild.id <= newElement.id) {
               let buildDefIndex = buildDefs.indexOf(buildDef, 0);
           
